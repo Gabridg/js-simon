0ea50bf8184aec7b857ac459aa5d0bf3d76419ac
Gabridg <gabri99.del@gmail.com>
2022-07-04 15:12:24 +0200

    brainstorming

--- js/script.js
+++ js/script.js
@@ -12,6 +12,15 @@ ATTENZIONE:
 prompt() e alert() vengono sempre eseguiti prima del resto del codice nel loro scope, quindi dovete trovare un escamotage per "tenerli a bada" finchè le altre operazioni siano state svolte. 
 Siete autorizzate a "imbrogliare" un po' e chiedere all'utente i numeri un pochettino dopo :faccia_leggermente_sorridente: l'importante è far sparire i numeri allo scadere dei 30 secondi!
 */
+
 /*
 MILESTONE 1:
-creare gli elementi HTML
+creare gli elementi HTML e CSS per visualizzare i 5 numeri casuali.
+Generare i numeri tramite JS.
+MILESTONE 2:
+far partire un timer di 30sec.
+chiedere all'utente tramite prompt quali erano i 5 numeri allo scadere dei 30sec.
+MILESTONE 3:
+vedere se i numeri inseriti dall'utente sono i numeri estratti casualmente.
+controllare quali numeri sono stati indovinati e quanti numeri.
+*/
